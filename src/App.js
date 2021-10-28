5dc4823b3fca884dc7c7877149beee8e2855e702
avinashluhana <aviluhana@gmail.com>
2021-10-28 02:32:25 -0700

    task done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './Components/Navbar';
-import Home from './Components/Home';
-import Footer from './Footer';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
-     <Navbar />
-     <Home />
-     <Footer />
+      <Navbar />
+      <Home />
+      <Footer />
     </div>
   );
 }
